6c7331496fa9a29398b15fb934b730582216db21
Subaa2128 <subaa86@gmail.com>
2022-06-10 14:14:41 +0530

    feat: navbar,product page,product details page,checkout page

---
+++ subaa/js/checkout.js
@@ -0,0 +1,33 @@
+const cancel = document.getElementById("cancel");
+const proceed = document.querySelector("#proceed");
+
+proceed.addEventListener("click", function () {
+  const cardHolder = document.getElementById("cardHolder").value;
+  const cardNumber = document.getElementById("cardNumber").value;
+  const expirationDate = document.getElementById("expirationDate").value;
+  const cardVerification = document.getElementById("cardVerification").value;
+
+  if (cardHolder === "") {
+    alert("card holder not filled");
+    return;
+  }
+  if (cardNumber === "") {
+    alert("card number not filled");
+    return;
+  }
+  if (expirationDate === "") {
+    alert("expiration date not filled");
+    return;
+  }
+  if (cardVerification === "") {
+    alert("card verification not filled");
+    return;
+  }
+  window.location.href =
+    "http://127.0.0.1:5500/subaa/Public/HTML/paymenysucess.html";
+});
+
+cancel.addEventListener("click", function () {
+  window.location.href =
+    "http://127.0.0.1:5500/subaa/Public/HTML/alljwelery.html";
+});
