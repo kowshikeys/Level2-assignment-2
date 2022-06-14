38423d151b3e4a94708dc49ee2220d3e9007c9b4
Subaa2128 <subaa86@gmail.com>
2022-06-14 16:38:17 +0530

    chore :home page navbar

--- js/home.js
+++ js/home.js
@@ -12,3 +12,14 @@ button.addEventListener("click", function () {
   sessionStorage.clear();
   window.location.href = "http://127.0.0.1:5500/Public/HTML/login.html";
 });
+
+const btn = document.querySelector(".close-nav-menu");
+const bar = document.querySelector(".navbar");
+
+function openMenu() {
+  bar.style.display = "block";
+}
+
+function closeMenu() {
+  bar.style.display = "none";
+}
