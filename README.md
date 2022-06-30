3f3bbe1389d407f3df380f84ebfa0e91d1794a23
kowshikeys <go2kowshiktouch@gmail.com>
2022-06-30 15:28:32 +0530

    feat:theme swither,filtering by price,readme

--- README.md
+++ README.md
@@ -4,20 +4,32 @@ Main purpose of the project is to make an ecommerce website.
 
 ## Login Signup
 
- Signup Login function is used in this project.Details are stored in local storage and session storage.
+ Signup Login function is used in this project.Details of the users such as Email,Name and Password are stored in local storage as well as  session storage.
 
-## AddTocart
+ ## Logout
 
-AddTocart function is used by clicking the cart button in the product card
+ Logout button in the navbar of Landing-page is used to remove the user details which is stored in session storage and Route you to the Login page
+
+## Product-page
+
+Products are stored in array of Object ,a product contains Id,Title,Description,price,Cart,Buy and catogery.
+
+## Addtocart
+
+In the Product list each card contains a button named Cart .The button is used to get the index from the product list and stores in the local-storage.By getting the index from the product list,it is set in a page called Cart page.By clicking the cart icon we can see the added product cart from the  list of products 
 
 ## Remove cart
 
-Remove cart function is used by clicking the remove button in the product card 
+The product list which contains a cart button once add to cart page , the cart button turns into "remove button" to clear the add product from the local storage as well as from the cart page
+
 
 ## Filter
+Filter is made for four catagory Gold,Diamond,Silver and Platinum .
+By clicking the Gold catagory You can filter the" gold " which is used in the title of the product list 
 
-Filter function is used by clicking the given options to filter the product from the products array
 
 ## Search
 
-Search function is used by clicking the search icon and type a product it automatically  filter the product from the products array
+
+
+
--- kowshik/javascript/landing-page.js
+++ kowshik/javascript/landing-page.js
@@ -13,11 +13,77 @@
 
 
 let mode = document.querySelector(".dark-mode");
- mode.addEventListener("click",() =>{
-     
-     document.body.classList.toggle("dark")
-  
- })
+mode.addEventListener("click", () => {
+
+  document.body.classList.remove("orange", "green", "sandal")
+
+  document.body.classList.toggle("dark")
+
+  if (document.body.classList.contains("dark")) {
+    colortheme.style.display = "none"
+    return;
+  }
+  colortheme.style.display = "flex"
+
+
+})
+
+//  blue color theme code 
+
+let blue = document.querySelector(".blue");
+blue.addEventListener("click", () => {
+  document.body.classList.remove("green", "sandal", "orange")
+  document.body.classList.toggle("blue")
+})
+
+
+
+
+//  orange color theme code 
+
+let orange = document.querySelector(".orange");
+orange.addEventListener("click", () => {
+  document.body.classList.remove("green", "sandal", "blue")
+  document.body.classList.toggle("orange")
+})
+
+//  green color theme code 
+
+let green = document.querySelector(".green");
+green.addEventListener("click", () => {
+  document.body.classList.remove("blue", "sandal", "orange")
+  document.body.classList.toggle("green")
+})
+
+//  sandal color theme code 
+
+let sandal = document.querySelector(".sandal");
+sandal.addEventListener("click", () => {
+  document.body.classList.remove("blue", "green", "orange")
+  document.body.classList.toggle("sandal")
+})
+
+
+
+// to target the button to show actived color
+
+let buttons = document.querySelectorAll(".btn");
+
+for (var button of buttons) {
+  button.addEventListener("click", (e) => {
+    let target = e.target;
+
+    // automatically close when trageted color is choosen
+
+    let open = document.querySelector(".open");
+    if (open) open.classList.remove("open");
+
+    document.querySelector(".active").classList.remove("active");
+    target.classList.add("active");
+
+  });
+}
+
 
 
 //  logout
@@ -30,18 +96,27 @@ btn.addEventListener("click", function () {
     "http://127.0.0.1:5500/kowshik/Public/Html/accountManagment.html?";
 });
 
+// //  open and close the theme box 
+
+let colortheme = document.querySelector(".color-theme");
+let coloricon = document.querySelector(".color-icons");
+
+coloricon.addEventListener("click", () => {
+  colortheme.classList.toggle("open");
+});
+
 
 const menubtn = document.querySelector(".buttonX");
 const bar = document.querySelector(".hamburger");
 const sidebars = document.querySelector(".sec-topbar");
 
-      function openMenu() {
-        sidebars.style.display = "flex";
-      }
+function openMenu() {
+  sidebars.style.display = "flex";
+}
 
-      function closeMenu() {
-        sidebars.style.display = "none";
-      }
+function closeMenu() {
+  sidebars.style.display = "none";
+}
 
 
 
